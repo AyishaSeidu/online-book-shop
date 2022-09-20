646a14be2de3561452f2f05858cb3a572fa47cbb
AyishaSeidu <ayishetuseidu13@gmail.com>
2022-09-20 17:25:15 +0000

    install dependecies for implementing http patch

--- OnlineBookShop.Api/Repositories/ICartRepo.cs
+++ OnlineBookShop.Api/Repositories/ICartRepo.cs
@@ -1,4 +1,5 @@
-﻿using OnlineBookShop.Api.Models;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using OnlineBookShop.Api.Models;
 using OnlineBookShop.Models.DTOs;
 
 namespace OnlineBookShop.Api.Repositories
@@ -6,7 +7,8 @@ namespace OnlineBookShop.Api.Repositories
     public interface ICartRepo
     {
         public Task<CartItem> AddItem(CartItemCreateDTO cartItem);
-        Task<CartItem> UpdateItemQuantity(int cartID, CartItemQtyUpdateDTO quantity);
+        Task<CartItem> UpdateItemQuantity(CartItemQtyUpdateDTO quantity);
+        Task<CartItem> UpdateItemQuantityPatch(int itemId, JsonPatchDocument quantity);
         Task<CartItem> DeleteItem(int cartItemID);  
         Task<CartItem> GetItem(int cartID);
         Task<IEnumerable<CartItem>> GetCartItems(int userID);

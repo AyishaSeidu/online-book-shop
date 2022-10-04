0d63c46380572a27390031a7f9a33246fbfffb3f
AyishaSeidu <ayishetuseidu13@gmail.com>
2022-10-04 13:40:51 +0000

    add genre http repository classes

---
+++ OnlineBookShop.Web/HttpRepositories/GenreHttpRepo.cs
@@ -0,0 +1,29 @@
+﻿using OnlineBookShop.Models.DTOs;
+using OnlineBookShop.Web.HttpRepositories.Contracts;
+using System.Net.Http.Json;
+
+namespace OnlineBookShop.Web.HttpRepositories
+{
+    public class GenreHttpRepo : IGenreHttpRepo
+    {
+        private HttpClient _httpClient;
+
+        public GenreHttpRepo(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<GenreReadDTO>> GetGenres()
+        {
+            try
+            {
+                var genres = await _httpClient.GetFromJsonAsync<IEnumerable<GenreReadDTO>>("api/genre");
+                return genres;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

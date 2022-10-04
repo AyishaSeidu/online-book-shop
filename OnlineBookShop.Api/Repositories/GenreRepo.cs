ca89a78f664f6f353498d9d1d4e266cfba935f06
AyishaSeidu <ayishetuseidu13@gmail.com>
2022-10-04 12:40:18 +0000

    add genre repository class

---
+++ OnlineBookShop.Api/Repositories/GenreRepo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookShop.Api.Data;
+using OnlineBookShop.Api.Models;
+
+namespace OnlineBookShop.Api.Repositories
+{
+    public class GenreRepo : IGenreRepo
+    {
+        private ShopDbContext _dbContext;
+
+        public GenreRepo(ShopDbContext shopDBContext)
+        {
+            _dbContext = shopDBContext;
+        }
+        public async Task<IEnumerable<Genre>> GetGenres()
+        {
+            return await _dbContext.Genres.ToListAsync();
+        }
+    }
+}

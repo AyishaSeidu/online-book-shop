770a656d5324f65f51f30f38980919deaf5ffaf3
AyishaSeidu <ayishetuseidu13@gmail.com>
2022-10-05 11:39:35 +0000

    add get book by genre id function to http repo

---
+++ OnlineBookShop.Web/Pages/Bases/BooksByGenre.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using OnlineBookShop.Models.DTOs;
+using OnlineBookShop.Web.HttpRepositories.Contracts;
+
+namespace OnlineBookShop.Web.Pages.Bases
+{
+    public class BooksByGenre : ComponentBase
+    {
+        [Parameter]
+        public int GenreId { get; set; }
+
+        [Inject]
+
+        public IBookHttpRepo BookHttpRepo { get; set; }
+
+        public IEnumerable<BookReadDTO> Books { set; get; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Books = await BookHttpRepo.GetBooksByGenreId(GenreId);
+            }
+            catch (Exception e)
+            {
+
+                ErrorMessage = e.Message;
+            }
+        }
+    }
+}
